0e75a3b4450538b663c221025d87cae521004c63
PJPhukan <paragjyotiphukan2004@gmail.com>
2024-04-11 12:44:38 +0530

    feat : " Home section completed "

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import './home.css'
 import Navber from '../../Components/Navber/Navber'
+import LeftSide from '../../Components/LeftSideContainer/LeftSide'
+import MainPost from '../../Components/MainPostContainer/MainPost'
+import RightSide from '../../Components/RightSideContainer/RightSide'
 const Home = () => {
     return (
-        <>
+        <div className='home'>
             <Navber />
-            <div>
+            <div className='componet-container'>
+                <LeftSide/>
+                <MainPost/>
+                <RightSide/>
 
-                This is Home page
             </div>
-        </>
+        </div>
     )
 }
 
